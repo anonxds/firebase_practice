830ee5747be66be77fb1785de83fc6dff3b82f33
Lenovo NVme <Lenovo NVme@DESKTOP-HMFJ1V0>
2021-06-25 21:54:58 -0700

    Agregar archivos de proyecto.

---
+++ firebase_practice/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace firebase_practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.resultado = new System.Windows.Forms.Button();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.lblx1 = new System.Windows.Forms.Label();
+            this.lblx2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(606, 381);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(791, 356);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(75, 31);
+            this.resultado.TabIndex = 1;
+            this.resultado.Text = "resultado";
+            this.resultado.UseVisualStyleBackColor = true;
+            this.resultado.Click += new System.EventHandler(this.resultado_Click);
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(791, 180);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 22);
+            this.txtc.TabIndex = 3;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(791, 133);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 22);
+            this.txtb.TabIndex = 4;
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(791, 89);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 22);
+            this.txta.TabIndex = 5;
+            // 
+            // lblx1
+            // 
+            this.lblx1.AutoSize = true;
+            this.lblx1.Location = new System.Drawing.Point(757, 256);
+            this.lblx1.Name = "lblx1";
+            this.lblx1.Size = new System.Drawing.Size(46, 17);
+            this.lblx1.TabIndex = 6;
+            this.lblx1.Text = "label1";
+            // 
+            // lblx2
+            // 
+            this.lblx2.AutoSize = true;
+            this.lblx2.Location = new System.Drawing.Point(845, 256);
+            this.lblx2.Name = "lblx2";
+            this.lblx2.Size = new System.Drawing.Size(46, 17);
+            this.lblx2.TabIndex = 7;
+            this.lblx2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 450);
+            this.Controls.Add(this.lblx2);
+            this.Controls.Add(this.lblx1);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button resultado;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Label lblx1;
+        private System.Windows.Forms.Label lblx2;
+    }
+}
+
---
+++ firebase_practice/Form1.cs
@@ -0,0 +1,132 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace firebase_practice
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "kHsT70sE7Q9hPkWFmhCFHxYGghrnUodOf9AG6B1W",
+            BasePath = "https://formulageneral-5b180.firebaseio.com/"
+        };
+        IFirebaseClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new FireSharp.FirebaseClient(config);
+            if (client != null)
+            {
+                dt.Columns.Add("fecha");
+                dt.Columns.Add("A");
+                dt.Columns.Add("B");
+                dt.Columns.Add("C");
+                dt.Columns.Add("x1");
+                dt.Columns.Add("x2");
+                dataGridView1.DataSource = dt;
+                grid();
+            }
+        }
+
+        private async void grid()
+        {
+            bool validate = true;
+            dt.Rows.Clear();
+            int i = 0;
+            FirebaseResponse response = await client.GetAsync("Counter/node");
+            counter obj = response.ResultAs<counter>();
+            int cnt = Convert.ToInt32(obj.cnt);
+
+            while (validate == true)
+            {
+                i++;
+                try
+                {
+
+                    FirebaseResponse resp2 = await client.GetAsync("Operaciones/" + i);
+                    variables obj2 = resp2.ResultAs<variables>();
+                    if (obj2 == null)
+                    {
+                        validate = false;
+                    }
+                    else
+                    {
+                        DataRow row = dt.NewRow();
+                        row["fecha"] = obj2.fecha;
+                        row["A"] = obj2.A;
+                        row["B"] = obj2.B;
+                        row["C"] = obj2.C;
+                        row["x1"] = obj2.x1;
+                        row["x2"] = obj2.x2;
+                        dt.Rows.Add(row);
+                    }
+
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private async void resultado_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse resp = await client.GetAsync("Counter/node");
+            counter get = resp.ResultAs<counter>();
+
+            variables var = new variables();
+            formula();
+            var Variables = new variables
+            {
+                id = (Convert.ToInt32((get.cnt) + 1)),
+                fecha = DateTime.Now.ToString("yyyy-MM-dd HH:mm"),
+                A = txta.Text,
+                B = txtb.Text,
+                C = txtc.Text,
+                x1 = lblx1.Text,
+                x2 = lblx2.Text
+
+            };
+
+            SetResponse response = await client.SetAsync("Operaciones/" + Variables.id, Variables);
+            variables result = response.ResultAs<variables>();
+            MessageBox.Show("Inserto");
+
+            var obj = new counter
+            {
+                cnt = Variables.id
+            };
+
+            SetResponse responses = await client.SetAsync("Counter/node", obj);
+        }
+
+        public void formula()
+        {
+            double x1, x2;
+            variables var = new variables();
+            var.setA(Convert.ToDouble(txta.Text));
+            var.setB(Convert.ToDouble(txtb.Text));
+            var.setC(Convert.ToDouble(txtc.Text));
+            x1 = (-var.getB() + (Math.Sqrt(Math.Pow(var.getB(), 2) - (4 * var.getA() * var.getC()))) / (2 * var.getA()));
+            var.setx2(-var.getB() - (Math.Sqrt(Math.Pow(var.getB(), 2) - (4 * var.getA() * var.getC()))) / (2 * var.getA()));
+
+            lblx1.Text = x1.ToString();
+            lblx2.Text = var.getx2().ToString();
+
+
+        }
+    }
+}
